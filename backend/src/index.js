78c14bb3c0236363621c0f0969374adf76dba590
alelex10 <elvizvida@gmail.com>
2025-04-03 07:33:26 -0300

    configure environment variables for client and API URLs, update CORS settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import { PORT } from "./config.js";
 import taskRoutes from "./routes/task.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const corsOptions = {
-	origin: "http://localhost:5173", // Reemplaza con la URL de tu frontend
+	origin: process.env.REACT_APP_CLIENT_URL, // Reemplaza con la URL de tu frontend
 };
 
 app.use(cors(corsOptions)); // Usa cors con las opciones configuradas
